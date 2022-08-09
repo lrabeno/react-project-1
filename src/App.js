a05503a21c452f6eccceb44013960e321a1137cb
Louis J Rabeno <lrabeno@gmail.com>
2022-08-08 21:31:36 -0400

    some useState stuff

--- src/App.js
+++ src/App.js
@@ -1,43 +1,89 @@
 import './App.css';
-import { User } from './User';
+// import { User } from './User';
+import { useState } from 'react';
 
 function App() {
-  const age = 17;
-  const isGreen = true;
-  const names = ['Pedro', 'Jake', 'Jessica', 'Mike', 'Dustin', 'Smoochy'];
-  const users = [
-    { name: 'Pedro', age: 21 },
-    { name: 'Jake', age: 25 },
-    { name: 'Jessica', age: 45 },
-  ];
+  // const age = 17;
+  // const isGreen = true;
+  // const names = ['Pedro', 'Jake', 'Jessica', 'Mike', 'Dustin', 'Smoochy'];
+  // const users = [
+  //   { name: 'Pedro', age: 21 },
+  //   { name: 'Jake', age: 25 },
+  //   { name: 'Jessica', age: 45 },
+  // ];
+  const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [showText, setShowText] = useState(true);
+  const [textColor, setTextColor] = useState('');
+  const [count, setCount] = useState(0);
 
-  const planets = [
-    { name: 'Mars', isGasPlanet: false },
-    { name: 'Earth', isGasPlanet: false },
-    { name: 'Jupiter', isGasPlanet: true },
-    { name: 'Venus', isGasPlanet: false },
-    { name: 'Neptune', isGasPlanet: true },
-    { name: 'Uranus', isGasPlanet: true },
-  ];
+  // const planets = [
+  //   { name: 'Mars', isGasPlanet: false },
+  //   { name: 'Earth', isGasPlanet: false },
+  //   { name: 'Jupiter', isGasPlanet: true },
+  //   { name: 'Venus', isGasPlanet: false },
+  //   { name: 'Neptune', isGasPlanet: true },
+  //   { name: 'Uranus', isGasPlanet: true },
+  // ];
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
+
+  const decreaseAge = () => {
+    setAge(age - 1);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
+  const setToZero = () => {
+    setCount(0);
+  };
 
   return (
     <div className="App">
-      <div>
+      Age: {age}
+      <button onClick={increaseAge}>Increase Age</button>
+      <button onClick={decreaseAge}>Decrease Age</button>
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
+      {showText ? (
+        <h1 style={{ color: textColor }}>hi my name is louieeee</h1>
+      ) : (
+        ''
+      )}{' '}
+      <button onClick={() => setShowText(!showText)}>Show/Hide</button>
+      <button
+        onClick={() => setTextColor(textColor === 'red' ? 'black' : 'red')}
+      >
+        Change color
+      </button>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+      <button onClick={setToZero}>Set to Zero</button>
+      {/* <div>
         {planets.map((planet, idx) => {
           return planet.isGasPlanet ? <h1 key={idx}>{planet.name}</h1> : '';
         })}
       </div>
       {users.map((user, idx) => {
         return <User key={idx} name={user.name} age={user.age} />;
-      })}
+      })} */}
       {/* Conditional Rendering */}
-
       {/* {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
       <h1 style={{ color: isGreen ? 'green' : 'red' }}>This Has Color</h1>
       {isGreen && <button>True green button</button>} */}
-
       {/* Passing props with functions below */}
-
       {/* <User name="Louis" age={36} email="louis@louis.com" />
       <User name="pedro" age={21} email="pedro@pedro.com" />
       <User name="mike" age={37} email="mike@mike.com" />
