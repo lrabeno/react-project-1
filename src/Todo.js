19abb11e4e1f0c3da1d3221716de523aa52d6b64
Louis J Rabeno <lrabeno@gmail.com>
2022-08-09 20:13:23 -0400

    can add todos

--- src/Todo.js
+++ src/Todo.js
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+
 export const Todo = () => {
-  return <div>TODO</div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
+  return (
+    <div>
+      todo
+      <div className="addTask">
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>Add task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task, idx) => {
+          return <h1 key={idx}>{task}</h1>;
+        })}
+      </div>
+    </div>
+  );
 };
