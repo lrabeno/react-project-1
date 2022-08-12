4589f9013fa0c26434b6396a00c025225285c7b4
Louis J Rabeno <lrabeno@gmail.com>
2022-08-12 12:18:37 -0400

    finished excuse file

--- src/Excuse.js
+++ src/Excuse.js
@@ -2,23 +2,24 @@ import axios from 'axios';
 import { useState } from 'react';
 
 export const Excuse = () => {
-  const [name, setName] = useState('');
-  const [familyExcuse, setFamilyExcuse] = useState(null);
+  const [excuse, setExcuse] = useState('');
 
-  const fetchFamilyExcuse = () => {
-    axios.get('https://excuser.herokuapp.com/v1/excuse/family/').then((res) => {
-      console.log(res.data[0].excuse);
-      setFamilyExcuse(res.data[0].excuse);
-    });
+  const fetchExcuse = (excuse) => {
+    axios
+      .get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`)
+      .then((res) => {
+        console.log(res.data[0].excuse);
+        setExcuse(res.data[0].excuse);
+      });
   };
   return (
     <div>
       <h1>Generate An Excuse</h1>
 
-      <button>Party</button>
-      <button onClick={fetchFamilyExcuse}>Family</button>
-      <button>Office</button>
-      <h1>Excuse: {familyExcuse}</h1>
+      <button onClick={() => fetchExcuse('party')}>Party</button>
+      <button onClick={() => fetchExcuse('family')}>Family</button>
+      <button onClick={() => fetchExcuse('office')}>Office</button>
+      <h1>Excuse: {excuse}</h1>
     </div>
   );
 };
