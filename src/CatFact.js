6c9641dc29293d995f8298dd466edf2dbe40cb10
Louis J Rabeno <lrabeno@gmail.com>
2022-08-11 22:00:59 -0400

    cat fact displaying

--- src/CatFact.js
+++ src/CatFact.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export const CatFact = () => {
+  //   fetch('https://catfact.ninja/fact')
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+
+  const [catFact, setCatFact] = useState('');
+
+  useEffect(() => {
+    console.log('cat fact');
+    axios.get('https://catfact.ninja/fact').then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }, []);
+
   return (
     <div>
       <button>Generate Cat Fact</button>
-      <p></p>
+      <p>{catFact}</p>
     </div>
   );
 };
