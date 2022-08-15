d35d11665ac2913fdb700586c1044e2665ba1abf
Louis J Rabeno <lrabeno@gmail.com>
2022-08-15 19:48:31 -0400

    errors handled on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,17 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const Home = () => {
-  const { data, isLoading } = useQuery(['cat'], () => {
+  const { data, isLoading, isError } = useQuery(['cat'], () => {
     return axios.get('https://catfact.ninja/fact').then((res) => res.data);
   });
 
   const { username } = useContext(AppContext);
   if (isLoading) {
     return <h1>Loading........</h1>;
+  }
+
+  if (isError) {
+    return <h1>Sorry there was an error</h1>;
   }
   return (
     <div>
