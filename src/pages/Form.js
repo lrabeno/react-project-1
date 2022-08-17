c1e4ef065db2ca3688c19b23dd963fe29b9ef0b1
Louis J Rabeno <lrabeno@gmail.com>
2022-08-16 20:45:28 -0400

    form schema set up

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,19 @@
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
 export const Form = () => {
   const { register, handleSubmit } = useForm();
+
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).max(20).required(),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), null])
+      .required(),
+  });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -16,7 +28,7 @@ export const Form = () => {
           {...register('fullName')}
         />
         <input type="text" placeholder="Email..." {...register('email')} />
-        <input type="text" placeholder="Age..." {...register('age')} />
+        <input type="number" placeholder="Age..." {...register('age')} />
         <input
           type="password"
           placeholder="Password..."
