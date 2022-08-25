bf10077f43c9b771456f4bea8487fdf878061ca1
Louis J Rabeno <lrabeno@gmail.com>
2022-08-25 17:53:48 -0400

    code in custom file

--- src/pages/Custom.js
+++ src/pages/Custom.js
@@ -1,3 +1,14 @@
+import { useState } from 'react';
+
 export const Custom = () => {
-  return <h1>Custom Page yo!!!</h1>;
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setIsVisible((prev) => !prev)}>
+        {isVisible ? 'Hide' : 'Show'}
+      </button>
+      {isVisible ? <h1>secret text!</h1> : ''}
+    </div>
+  );
 };
